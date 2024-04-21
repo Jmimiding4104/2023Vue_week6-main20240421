8f3fd7f65245310fa9e04fbe87fdc9d29c09ac6b
Jmimiding4104@gmail.com <Jmimiding4104>
2024-04-21 19:52:55 +0800

    Updates

--- assets/AboutView-DnPQ2wMu.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as e,o}from"./index-DHS3S8S5.js";const n={data(){return{}},mounted(){}},s={class:"d-flex justify-content-center",style:{"margin-top":"50px"}};function c(r,a,_,p,d,f){return o(),e("h2",s,"關於我們")}const u=t(n,[["render",c]]);export{u as default};
--- assets/CouponView-xP6EPLY1.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as t}from"./index-DHS3S8S5.js";const o={},n={class:"d-flex justify-content-center"};function s(r,_){return t(),c("h2",n,"優惠卷列表")}const f=e(o,[["render",s]]);export{f as default};
